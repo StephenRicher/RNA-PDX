ac92879acbac374a6bcd3049a15e886781232769
Stephen Richer <sr467@bath.ac.uk>
2022-04-19 09:17:21 +0100

    Update.

---
+++ sleuth/4.convertToBigBed.sh
@@ -0,0 +1,2 @@
+bedToBigBed -type=bed12  treatment-diffTx.bed12 ../genome/grch38.chrom.sizes treatment-diffTx.bb
+bedToBigBed -type=bed12  response-diffTx.bed12 ../genome/grch38.chrom.sizes response-diffTx.bb
